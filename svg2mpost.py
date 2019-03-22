1cb2a011b0c8027082f15a68717e74dd942908d4
Antoine-Gelgon <antoine@luuse.io>
2019-03-23 00:49:44 +0100

    ça marche presque

--- svg2mpost.py
+++ svg2mpost.py
@@ -1,8 +1,8 @@
 from svg.path import parse_path
 
-d = 'M 15,5 V 22 M 15,11 13,9 11,8 H 8 L 6,9 4,11 3,14 v 2 l 1,3 2,2 2,1 h 3 l 2,-1 2,-2'
+d = 'M 4,1 V 22 M 5,1 v 21 m 5,-21 3,1 2,2 1,2 v 2 l -1,2 -2,1 -3,1 m -5,0 h 5 l 4,-1 2,-1 1,-2 V 6 L 16,4 14,2 10,1 H 2 3 L 4,2 M 4,21 3,22 H 2 7 6 L 5,21 m 6,-9 1,3 1,2 2,3 2,2 -3,-2 -2,-3 -1,-2 -1,-3'
 
-
+y = 40
 def parsePath(path):
     dparse = parse_path(path)
     print(dparse)
@@ -13,41 +13,45 @@ def parsePath(path):
     for point in dparse:
         if str(point)[0:4] == 'Move':
             if incD > 0:
+                cordonates.append('x' + str(inc) + ' = ' + str(dparse[inc].end.real) + 'ux;')
+                cordonates.append('y' + str(inc) + ' = ' + str(dparse[inc].end.imag) + 'uy;')
+                draws.append(' --')
+                draws.append(' z' + str(inc))
                 draws.append('; \n')
+                inc = inc + 1
 
             draws.append('draw')
             incD = 1
-            inc = 0
+            ii = 0
 
         elif str(point)[0:4] == 'Line': 
 
             cordonates.append('x' + str(inc) + ' = ' + str(point.start.real) + 'ux;')
             cordonates.append('y' + str(inc)+ ' = ' + str(point.start.imag) + 'uy;')
-            cordonates.append('x' + str(inc + 1) + ' = ' + str(point.end.real) + 'ux;')
-            cordonates.append('y' + str(inc + 1)+ ' = ' + str(point.end.imag) + 'uy;')
+            # cordonates.append('x' + str(inc + 1) + ' = ' + str(point.end.real) + 'ux;')
+            # cordonates.append('y' + str(inc + 1)+ ' = ' + str(point.end.imag) + 'uy;')
  
-            if inc > 0:
+            if ii > 0:
                 draws.append(' --')
 
             draws.append(' z' + str(inc))
-            draws.append(' --')
-            draws.append(' z' + str(inc + 1))
+            inc = inc + 1
+            # draws.append(' --')
+            # draws.append(' z' + str(inc + 1))
             
-            inc = inc + 2
+            # inc = inc + 1
+            ii = 1
         elif str(point)[0:4] == 'Close': 
             draws.append(';')
+    cordonates.append('x' + str(inc) + ' = ' + str(dparse[inc].end.real) + 'ux;')
+    cordonates.append('y' + str(inc) + ' = ' + str(dparse[inc].end.imag) + 'uy;')
+    draws.append(' --')
+    draws.append(' z' + str(inc))
+    draws.append('; \n')
 
     return [cordonates, draws]
 
 valueP = parsePath(d)  # [0] ) cordonates; [1] draws
-
-# print(''.join(valueP[1]))
-
-''.join(valueP[0])
-# for v in valueP[0]:
-#     print(v)
-#     
-
 
 
 buildFig = '''
@@ -56,7 +60,7 @@ ux = 1;
 uy = 1;
 beginfig({keycode}):
     {cordonates} 
-    {draws};
+    {draws}
 endfig;
 
 end;
