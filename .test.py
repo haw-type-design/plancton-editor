b473cb4c39a4afcfafc222b81d17b2fb0ca410aa
Antoine-Gelgon <antoine@luuse.io>
2019-04-24 23:44:34 +0200

    [cleanage] -> files/output-svg

--- .test.py
+++
@@ -1,9 +0,0 @@
-import lib.svg2mpost as svg2mp
-
-# svg2mp.buildMp('files/input-svg/', 'files/mpost/mpost-files/')
-
-
-svg2mp.buildSvg('files/mpost/mpost-files/', '-all')
-
-# svg2mp.buildSvg('files/mpost/mpost-files/')
-# svg2mp.buildGlobalMp('files/global.json')
